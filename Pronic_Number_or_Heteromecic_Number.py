78255913657cd3c1bf6028d2fb2a4fd74330220e
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-02 21:10:49 +0530

    n=int(input()) for i in range(1,n//2):     if(n%i==0):         if(abs(i-(n/i))==1):             print("YES")             break else:     print("NO")

--- Pronic_Number_or_Heteromecic_Number.py
+++ Pronic_Number_or_Heteromecic_Number.py
@@ -1,8 +1,9 @@
 n=int(input())
-for i in range(1,int(n**(0.5))+1):
+for i in range(1,n//2):
     if(n%i==0):
-        if(n//i == i+1):
+        if(abs(i-(n/i))==1):
             print("YES")
             break
 else:
     print("NO")
+        
