f3e5f4c93adb40dadb9496b236a234934c890b6c
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-03 10:32:39 +0530

    def isprime(p):     if p>1:         for i in range(2,int(p**0.5)+1):             if p%i==0:                 break         else:             return 1     else:         return 1 n=int(input()) n1=int(input()) c=n+n1 c1=1 while 1:     c=c+1     if isprime(c):         break     else:         c1+=1 print(c1)

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,18 +1,22 @@
-def prime(p):
-    if(p>1):
-        for i in range(2,int(p**(0.5))+1):
-            if(p%i==0):
-                return(0)
+def isprime(p):
+    if p>1:
+        for i in range(2,int(p**0.5)+1):
+            if p%i==0:
+                break
         else:
-            return(1)
+            return 1
     else:
-        return(1)
+        return 1
+n=int(input())
 n1=int(input())
-n=n1+int(input())
-c=1
-while(1):
-    if(prime(n+c)):
-        print(c)
+c=n+n1
+c1=1
+while 1:
+    c=c+1
+    if isprime(c):
         break
     else:
-        c+=1
+        c1+=1
+print(c1)
+        
+    
