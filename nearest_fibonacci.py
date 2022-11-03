50fa9d06f74df1d6c0f73cf6075e93ba8ad00aff
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-03 22:07:47 +0530

    n=int(input()) a=0 b=1 c=a+b for i in range(1,n+1):     a=b     b=c     c=a+b     if abs(b-n)<abs(c-n):         print(b)         break     elif abs(b-n)>abs(c-n):         continue     else:         print(b,c)         break

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -2,13 +2,15 @@ n=int(input())
 a=0
 b=1
 c=a+b
-while(c<=n):
+for i in range(1,n+1):
     a=b
     b=c
     c=a+b
-if(abs(b-n)==abs(c-n)):
-    print(b,c)
-elif(abs(b-n)<abs(c-n)):
-    print(b)
-else:
-    print(c)
+    if abs(b-n)<abs(c-n):
+        print(b)
+        break
+    elif abs(b-n)>abs(c-n):
+        continue
+    else:
+        print(b,c)
+        break
