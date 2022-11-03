a6dbe63c27f674f654a49dcbfb48b86f3634ac0e
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-03 20:46:26 +0530

    def isprime(p):     if p>1:         for i in range(2,int(p**0.5)+1):             if p%i==0:                 break         else:             return 1     else:         return 0 n=int(input()) f=False if isprime(n):     a=str(n)     for i in a:         if isprime(int(i)):             f=True         else:             f=False             break     if f:         print("Mega Prime")     else:         print("Not Mega Prime") else:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,22 +1,25 @@
-def prime(p):
-    if(p>1):
-        for i in range(2,int(p**(0.5))+1):
-            if(p%i==0):
-                return(0)
+def isprime(p):
+    if p>1:
+        for i in range(2,int(p**0.5)+1):
+            if p%i==0:
+                break
         else:
-            return(1)
+            return 1
     else:
-        return(0)
-        
+        return 0
 n=int(input())
-if(prime(n)):
-    while(n>0):
-        r=n%10
-        if(prime(r)==0):
-            print("Not Mega Prime")
+f=False
+if isprime(n):
+    a=str(n)
+    for i in a:
+        if isprime(int(i)):
+            f=True
+        else:
+            f=False
             break
-        n=n//10
-    else:
+    if f:
         print("Mega Prime")
+    else:
+        print("Not Mega Prime")
 else:
     print("Not Mega Prime")
