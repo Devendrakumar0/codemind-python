b0db36a8a0d47a6e7fee0278deeadcf9c88aaac2
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-03 22:20:36 +0530

    def checkDivisibility(n, digit) : 	return (digit != 0 and n % digit == 0) def allDigitsDivide( n) : 	temp = n 	while (temp > 0) : 		digit = temp % 10 		if ((checkDivisibility(n, digit)) == False) : 			return False 		temp = temp // 10 	return True n=int(input()) n1=int(input()) for i in range(n,n1+1):     if allDigitsDivide(i):         print(i,end=" ")

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,17 +1,15 @@
-def che(p):
-    n=p
-    while(p>0):
-        r=p%10
-        if(r==0):
-            return(0)
-        elif(n%r!=0):
-            return(0)
-        p=p//10
-    else:
-        return(1)
-    
-a=int(input())
-b=int(input())
-for i in range(a,b+1):
-    if(che(i)):
+def checkDivisibility(n, digit) :
+	return (digit != 0 and n % digit == 0)
+def allDigitsDivide( n) :
+	temp = n
+	while (temp > 0) :
+		digit = temp % 10
+		if ((checkDivisibility(n, digit)) == False) :
+			return False
+		temp = temp // 10
+	return True
+n=int(input())
+n1=int(input())
+for i in range(n,n1+1):
+    if allDigitsDivide(i):
         print(i,end=" ")
