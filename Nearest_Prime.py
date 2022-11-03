0240dd1b088175dcd7369e1af4324126aa2e28bc
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-03 19:35:06 +0530

    def isprime(p):     if p>1:         for i in range(2,int(p**0.5)+1):             if p%i==0:                 break         else:             return 1     else:         return 1 for _ in range(int(input())):     a=int(input())     b=a     c=a     while 1:         if isprime(b):             break         else:             b=b-1     while 1:         if isprime(c):             break         else:             c=c+1     d=abs(b-a)     d1=abs(c-a)     if d>d1:         print(c)     elif d1>d:         print(b)     else:         print(min(b,c))

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,26 +1,32 @@
-def prime(p):
-    if(p>1):
-        for i in range(2,int(p**(0.5))+1):
-            if(p%i==0):
-                return(0)
+def isprime(p):
+    if p>1:
+        for i in range(2,int(p**0.5)+1):
+            if p%i==0:
+                break
         else:
-            return(1)
+            return 1
     else:
-        return(0)
+        return 1
 for _ in range(int(input())):
     a=int(input())
-    if(prime(a)):
-        print(a)
+    b=a
+    c=a
+    while 1:
+        if isprime(b):
+            break
+        else:
+            b=b-1
+    while 1:
+        if isprime(c):
+            break
+        else:
+            c=c+1
+    d=abs(b-a)
+    d1=abs(c-a)
+    if d>d1:
+        print(c)
+    elif d1>d:
+        print(b)
     else:
-        b=a-1
-        c=a+1
-        while(1):
-            if(prime(b)):
-                print(b)
-                break
-            elif(prime(c)):
-                print(c)
-                break
-            else:
-                b-=1
-                c+=1
+        print(min(b,c))
+    
