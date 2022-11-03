76ea92fc0c7544d0863c7802956b5c7f4ad7fd5c
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-03 10:40:15 +0530

    def isprime(p):     if p>1:         for i in range(2,int(p**0.5)+1):             if p%i==0:                 break         else:             return 1     else:         return 1 n=int(input()) a=str(n) b=str(n)[::-1] a=int(a) b=int(b) if isprime(a):     if isprime(b):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,18 +1,19 @@
-def prime(i):
-    if(i>1):
-        for j in range(2,int(i**(0.5))+1):
-            if(i%j==0):
-                return(0)
+def isprime(p):
+    if p>1:
+        for i in range(2,int(p**0.5)+1):
+            if p%i==0:
+                break
         else:
-            return(1)
+            return 1
     else:
-        return(0)
-
+        return 1
 n=int(input())
-a=(str(n)[::-1])
-n1=int(a)
-if(prime(n)):
-    if(prime(n1)):
+a=str(n)
+b=str(n)[::-1]
+a=int(a)
+b=int(b)
+if isprime(a):
+    if isprime(b):
         print("circular prime")
     else:
         print("prime but not a circular prime")
