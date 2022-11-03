f883814c9119748935f2cd193f0ad8bb5dcd9f1c
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-03 21:27:25 +0530

    a,b=map(int,input().split()) li=[] c=0 for i in range(1,a+1):     if a%i==0:         li.append(i) for j in range(1,b+1):     if b%j==0:         if j in li:             c=j print(c)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,9 +1,11 @@
-def gcd(a,b):
-    if(a==0):
-        return(b)
-    else:
-        if(a<b):
-            a,b=b,a
-        return(gcd(a%b,b))
 a,b=map(int,input().split())
-print(gcd(a,b))
+li=[]
+c=0
+for i in range(1,a+1):
+    if a%i==0:
+        li.append(i)
+for j in range(1,b+1):
+    if b%j==0:
+        if j in li:
+            c=j
+print(c)
