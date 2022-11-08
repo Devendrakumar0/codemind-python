696c4ead7b964f2f1535c1ed5a958d810aec95c3
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-08 20:53:22 +0530

    s1=input() s1=s1.casefold() l1=list(s1.split()) a=[] for i in l1[0]:     for j in l1[1::]:         if(i not in j):             break     else:         a.append(i) a=sorted(a) if(len(a)==0):     print(-1) else:     print(a[0])

---
+++ Common_characters_-_3.py
@@ -0,0 +1,16 @@
+s1=input()
+s1=s1.casefold()
+l1=list(s1.split())
+a=[]
+for i in l1[0]:
+    for j in l1[1::]:
+        if(i not in j):
+            break
+    else:
+        a.append(i)
+a=sorted(a)
+if(len(a)==0):
+    print(-1)
+else:
+    print(a[0])
+
