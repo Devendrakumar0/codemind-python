df7a75ff2f213a2a3ef36b215cf7788611d6e60e
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-08 19:25:32 +0530

    def prime(p):     if(p>1):         for i in range(2,int(p**(0.5))+1):             if(p%i==0):                 return(0)                 break         else:             return(1)     else:         return(0) n=int(input()) l=list(map(int,input().split())) l=sorted(l) k=int(input()) c=0 for i in l:     if(i<=k):         if(prime(i)==1):             c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,20 @@
+def prime(p):
+    if(p>1):
+        for i in range(2,int(p**(0.5))+1):
+            if(p%i==0):
+                return(0)
+                break
+        else:
+            return(1)
+    else:
+        return(0)
+n=int(input())
+l=list(map(int,input().split()))
+l=sorted(l)
+k=int(input())
+c=0
+for i in l:
+    if(i<=k):
+        if(prime(i)==1):
+            c+=1
+print(c)
