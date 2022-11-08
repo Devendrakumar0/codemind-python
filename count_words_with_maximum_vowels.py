0ceafe60b49807372f06086e131cbf0bb2b30526
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-08 19:59:37 +0530

    def vow(p):     c=0     for i in p:         if i in "AEIOUaeiou":             c+=1     return(c)
    n=input()
    l=list(n.split())
    a=[]
    for i in l:
        a.append(vow(i))
    print(a.count(max(a)))

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,14 @@
+def vow(p):
+    c=0
+    for i in p:
+        if i in "AEIOUaeiou":
+            c+=1
+    return(c)
+
+n=input()
+l=list(n.split())
+a=[]
+for i in l:
+    a.append(vow(i))
+print(a.count(max(a)))
+    
