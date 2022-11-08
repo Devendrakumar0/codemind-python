37f0f810f6d67f56142d4c012d7ad909f17302a8
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-08 20:48:34 +0530

    s1=input() s2=input() s1=s1.casefold() s2=s2.casefold() a=[] for i in s1:     if(i!=" "):         if(i in s2)and(i not in a):             a.append(i) a=sorted(a) if(len(a)==0):     print(-1) else:     [print(i,end="")for i in a]

---
+++ common_characters.py
@@ -0,0 +1,14 @@
+s1=input()
+s2=input()
+s1=s1.casefold()
+s2=s2.casefold()
+a=[]
+for i in s1:
+    if(i!=" "):
+        if(i in s2)and(i not in a):
+            a.append(i)
+a=sorted(a)
+if(len(a)==0):
+    print(-1)
+else:
+    [print(i,end="")for i in a]
