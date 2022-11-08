1d91583934d60bb10c652629ff35cc6ff804d059
Devendrakumar0 <84584733+Devendrakumar0@users.noreply.github.com>
2022-11-08 20:44:53 +0530

    s=input() s=s.casefold() a=[] for i in s:     if(i!=" "):         if(i not in a):             a.append(i) a=sorted(a) [print(i,end="")for i in a]

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,16 +1,9 @@
 s=input()
 s=s.casefold()
 a=[]
-b=[]
 for i in s:
     if(i!=" "):
         if(i not in a):
             a.append(i)
-for i in a:
-    if(s.count(i)==1):
-        b.append(i)
-b=sorted(b)
-if(len(b)==0):
-    print(-1)
-else:
-    [print(i,end='')for i in b]
+a=sorted(a)
+[print(i,end="")for i in a]
